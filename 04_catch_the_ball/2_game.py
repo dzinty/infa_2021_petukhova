3c5bd430fdf98c5f52f7e34759205c932459855e
Dzinty <xenia.nice@gmail.com>
2021-10-08 17:13:56 +0300

    Реализовано исчезновение шариков после попадания

--- 04_catch_the_ball/2_game.py
+++ 04_catch_the_ball/2_game.py
@@ -18,14 +18,14 @@ balls = [] #массив координат шариков
 N = 0 #количество очков
 
 def new_ball():
-    '''рисует новый шарик и возвращает его координаты'''
+    '''рисует новый шарик и возвращает его координаты и цвет'''
     global x, y, r
     x = randint(100, 800)
     y = randint(100, 500)
     r = randint(10, 100)
     color = COLORS[randint(0, 5)]
     circle(screen, color, (x, y), r)
-    return x, y, r
+    return (x, y, r), color
 
 def mouse_xy(event):
     '''Возвращает координаты мыши'''
@@ -33,6 +33,14 @@ def mouse_xy(event):
     Y = event.pos[1]
     return X, Y
 
+def draw_ball(params):
+    '''
+    Рисует шарик по его параметрам (цвет, x, y, r)
+    '''
+    color = balls[i][1]
+    (X, Y) = (balls[i][0][0], balls[i][0][1])
+    r = balls[i][0][2]
+    circle(screen, color, (X, Y), r)
 
 def check_click(xyr, xy):
     '''
@@ -60,15 +68,20 @@ while not finished:
             #print(check_click(balls[len(balls)-1], mouse_xy(event)))
             #print(balls[len(balls)-1])
             #print(mouse_xy(event))
-            for i in range(len(balls)):
-                if check_click(balls[i], mouse_xy(event)):
+            i = 0
+            while i < len(balls):
+                if check_click(balls[i][0], mouse_xy(event)):
                     N += 1
                     print(N)
-    balls = []
-    for i in range(5):
+                    balls.pop(i)
+                i += 1
+    #balls = []
+    for i in range(1):
         balls.append(new_ball())
     pygame.display.update()
     screen.fill(BLACK)
+    for i in range(len(balls)):
+        draw_ball(balls[i])
 
 
 pygame.quit()
