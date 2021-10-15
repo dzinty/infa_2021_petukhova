295aa9140a5ad558417ea549f127f108569d36ed
Dzinty <xenia.nice@gmail.com>
2021-10-15 17:47:09 +0300

    Добавлен интерфейс ввода имени и вывода таблицы результатов в pygame.

--- 04_catch_the_ball/2_game.py
+++ 04_catch_the_ball/2_game.py
@@ -127,14 +127,70 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
+# ввод имени игрока
+pygame.font.init()
+f1 = pygame.font.Font(None, 36)
+text1 = f1.render('Введите имя игрока:', True, (255, 255, 255))
+
+
+font = pygame.font.Font(None, 32)
+input_box = pygame.Rect(10, 100, 140, 32)
+color_inactive = pygame.Color('lightskyblue3')
+color_active = pygame.Color('dodgerblue2')
+color = color_inactive
+active = False
+text = ''
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if input_box.collidepoint(event.pos):
+                # Toggle the active variable.
+                active = not active
+            else:
+                active = False
+            # Change the current color of the input box.
+            color = color_active if active else color_inactive
+        if event.type == pygame.KEYDOWN:
+            if active:
+                if event.key == pygame.K_RETURN:
+                    name = text
+                    print(text)
+                    text = ''
+                    done = True
+                elif event.key == pygame.K_BACKSPACE:
+                    text = text[:-1]
+                else:
+                    text += event.unicode
+    screen.fill((30, 30, 30))
+    # Render the current text.
+    txt_surface = font.render(text, True, color)
+    # Resize the box if the text is too long.
+    width = max(200, txt_surface.get_width()+10)
+    input_box.w = width
+    # Blit the text.
+    screen.blit(text1, (10, 50))
+    screen.blit(txt_surface, (input_box.x+5, input_box.y+5))
+    # Blit the input_box rect.
+    pygame.draw.rect(screen, color, input_box, 2)
+    pygame.display.flip()
+    clock.tick(30)
+
+
+
+
+#Игра
+
 time = 0
 game_time = 500
 
 alive = True
 color, x, y, r = new_square()
 
-name = input('Введите имя игрока:')
-
+pygame.init()
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -176,9 +232,31 @@ while not finished:
         finished = True
         print('Total score:', N)
 
+
+# Вывод таблицы с результатами
 table = open('table.txt', 'w')
 print(table_old, file=table)
 print(name, N, file=table)
 table.close()
+
+
+table = open('table.txt', 'r')
+data = table.readlines()
+data = [line.rstrip() for line in data]
+table.close()
+
+screen.fill(BLACK)
+text = []
+for line in data:
+    text.append(f1.render(line, True, (255, 255, 255)))
+finished = False
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+    for i in range(len(text)):
+        screen.blit(text[i], (10, 50 + 12*i))
+    pygame.display.flip()
 
 pygame.quit()
